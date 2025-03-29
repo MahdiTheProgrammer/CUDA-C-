21c9cab9f2939a298668a0b27eb1e006acb99d61
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-03-29 18:49:18 +0200

    commit

--- CNN/utils.h
+++ CNN/utils.h
@@ -1,1 +1,25 @@
 //Helper functions (e.g. random init, printing, etc.)
+
+// I will create a Tensor datatype here.
+
+
+struct Tensor{
+	std::vector<int> shape;
+	std::vector<int> stride;
+	std::vector<float> host_data;
+	float* device_data = nullptr;
+
+	Tensor(const std::vector<int>& shape_) shape(shape_){
+	}
+	~Tensor(){
+	}
+	void to_device(){
+	}
+	void to_host(){
+	}
+	int flatten_index(){
+	}
+	//Somehow accessing the data on host should be added
+
+
+}
