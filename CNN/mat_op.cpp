05d54a9de312f460ddea9e16a6f199f455ddfa74
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-03-31 20:36:15 +0300

    Working on MatrixMultiplication

--- CNN/mat_op.cpp
+++ CNN/mat_op.cpp
@@ -7,9 +7,7 @@
 Tensor Tensor::matmul(const Tensor& t_A, const Tensor& t_B){
 	shape_A = t_A.get_shape();
 	shape_B = t_B.get_shape();
-
 	int d=1;
-
 	for(itn f1=0; f1<shape.size()-2;f1++){
 		d*=shape_A[f1];
 	}
@@ -17,14 +15,15 @@ Tensor Tensor::matmul(const Tensor& t_A, const Tensor& t_B){
 	t_B.to_device();
 	add_A = t_A.device_address();
 	add_B = t_B.device_address();
-
 	int total_size_C = d * shape_B[shape_B.size()-1] * shape_A[shape_A.size()-2];
 	float *add_C;
+	float *h_C = new float[d * shape_B[shape_B.size()-1] * shape_A[shape_A.size()-2];
 	cudaMalloc((void**)&add_C,total_size_C * sizeof(float));
-
 	dim3 blockDim(32,32);
 	dim3 gridDim((shape_B[shape_B.size()-1]+31)/32,(shape_A[shape_A.size()-2]+31)/32 , d);
-	matrixmultiplication<<<gridDim, blockDim>>>(add_A,add_B,add_C, );
+	matrixmultiplication<<<gridDim, blockDim>>>(add_A,add_B,add_C,shape_A,shape_B);
+	cudaDeviceSynchronize();
+	cudaMemcpy(h_C, add_C, total_size_C * sizeof(float), cudaMemcpyHostToDevice);
 	return t_C;
 }
 
--- CNN/mat_op.cu
+++ CNN/mat_op.cu
@@ -2,17 +2,17 @@
 
 
 
-__global__ void matmul(float *t_A, float *t_B, float *c, std::vecotr<int>& s_A, std::vector<int> ){
+__global__ void matmul(float *t_A, float *t_B, float *c, std::vecotr<int>& shape_A, std::vector<int> shape_B){
+	int batch_id = blockIdx.z;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
-
+	int i = (batch_id * shape_A[shape_A.size()-2)] * shape_B[shape_B.size()-1)]) + row * shape_B[shape_B.size()-1] + col;
 	int e=0;
-	if (row < x && col < y){
-		for(int f1=0; f1<m;f1++){
-			e += a[m*row +f1] * b[col + f1*y];
+	if (row < shape_A[shape_A.size()-2] && col < shape_B[shape_B.size-1]){
+		for(int f1=0; f1<shape_A[shape_A.size()-1];f1++){
+			e += t_A[(batch_id * shape_A[shape_A.size()-2)] * shape_A[shape_A.size()-1)]) + shape_A[shape_A.size()-1]*row +f1] * t_B[(batch_id * shape_B[shape_B.size()-2)] * shape_B[shape_B.size()-1)]) + (shape_B[shape_B.size()-1]*f1) + col]
 		}
-		c[y*row + col] = e; 
+		c[i] = e;
 	}
 }
 
-//this only supports 2d tensros atm, but it will be soon working with any dim.
