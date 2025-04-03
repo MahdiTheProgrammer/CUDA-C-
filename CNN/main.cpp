482f547f1a858457e97b9e1cea9688dbebee94b5
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-04-03 13:21:18 +0300

    commit

--- CNN/main.cpp
+++ CNN/main.cpp
@@ -3,19 +3,19 @@
 #include "utils.h"
 
 int  main(){
-	std::vector<int> shape_A = {3,2,2};
-	std::vector<int> shape_B = {3,2,1};
-	std::vector<int> shape_C = {3,2,1};
+	std::vector<int> shape_A = {4,3,10,12};
+	std::vector<int> shape_B = {4,3,12,100};
+	std::vector<int> shape_C = {4,3,10,100};
 	Tensor t_A(shape_A);
 	Tensor t_B(shape_B);
 	Tensor t_C(shape_C);
-//	std::vector<float> i_a = {1.0f, 2.0f, 3.0f, 4.0f};
-	std::vector<float> i_b = {2.0f, 2.0f,2.0f,2.0f,2.0f,2.0f};
+//	std::vector<float> i_a = {1.0f, 2.0f, 3.0f, 4.0f};;
 	t_A.to_device();
 	t_B.to_device();
 	t_A.arrange(1,1);
+	t_B.arrange(0,2);
 //	t_A.from_list(i_a.data());
-	t_B.from_list(i_b.data());
+//	t_B.from_list(i_b.data());
 	t_A.print();
 	std::cout<<"\n";
 	t_B.print();
