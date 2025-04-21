e7895a135399e4a843fec7a9ff2c55a3e38b9378
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-04-21 20:55:34 +0300

    working on maxpool2d forward pass

--- CNN/maxpool2d.cu
+++ CNN/maxpool2d.cu
@@ -1,7 +1,21 @@
 #include <layer.h>
+#include <cuda_runtime.h>
 
-__global__ void maxpool2d(){
+__global__ void maxpool2d(int batch_size, float* input, float* output,int kernel,int width_out, int height_out ){
 
+	z = blockIdx.z;
+	row = blockIdx.y * blockDim.y + threadIdx.y;
+	col = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(row<height_out && col<width_out){
+	float l;
+		for(int h=0; h<kernel; h++){
+			for(int w=0; w<kernel; w++){
+				//
+			}
+		}
+	output[(z * height_out * width_out)+ (row*width_out) + col] = l;
+	}
 
 }
 
@@ -37,7 +51,7 @@ Tensor MaxPool2D::forward(Tensor& input){
 
         dim3 blockDim(32,32);
         dim3 gridDim((width_out+31)/32,(height_out+31)/32, num_outputs);
-        convolution<<<gridDim, blockDim>>>(batch_size, add_output, input_dim, num_outputs, height_out, width_out, height_in, width_in,  kernel, stride);
+        convolution<<<gridDim, blockDim>>>(batch_size, add_X, add_output, input_dim, num_outputs, height_out, width_out, height_in, width_in,  kernel, stride);
         cudaError_t err = cudaGetLastError();
         if (err != cudaSuccess) {
                 std::cerr << "CUDA kernel failed: " << cudaGetErrorString(err) << std::endl;
