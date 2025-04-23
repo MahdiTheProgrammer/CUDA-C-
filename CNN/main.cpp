21a86c704975aedb6d91b5f050c8c47e1ac00969
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-04-23 12:15:47 +0300

    commit

--- CNN/main.cpp
+++ CNN/main.cpp
@@ -7,41 +7,41 @@
 
 int  main(){
 //	std::vector<int> shape_A = {2,2,6,6};
-	std::vector<int> shape_B = {1,1,8,8};
-//	std::vector<int> shape_C = {2,2,6,6};
+	std::vector<int> shape_B = {2,4,8};
+	std::vector<int> shape_C = {1,8};
 //	Tensor t_A(shape_A);
 	Tensor t_B(shape_B);
-//	Tensor t_C(shape_C);
+	Tensor t_C(shape_C);
 //	std::vector<float> i_a = {1.0f, 2.0f, 3.0f, 4.0f};;
 //	t_A.to_device();
 //	t_B.to_device();
-	t_B.arrange(1.0,1.0);
-//	t_C.arrange(1.001,0.001);
+	t_B.arrange(1.0,0.0);
+	t_C.arrange(1.0,0.0);
 //	t_B.arrange(0,2);
 //	t_A.from_list(i_a.data());
 //	t_B.from_list(i_b.data());
-//	t_A.print();
-//	std::cout<<"\n";
+	t_B.print();
+	std::cout<<"\n";
 //	t_C.add_padding(1,0);
-//	t_C.print();
+	t_C.print();
 	std::cout<<"\n";
 //    	std::vector<int> shape = t_A.get_shape();
 //	std::cout<<"\n";
 //   	 for (int num : shape) {
 //        	std::cout << num << " ";
 //   	 }
-	Model model;
-	model.add(new flatten());
-	Tensor output = model.forward(t_B);
-	output.print();
+//	Model model;
+//	model.add(new flatten());
+//	Tensor output = model.forward(t_B);
+//	output.print();
 
 //	t_A.add_padding(1,0.0f);
 //	t_A.print();
 //	std::cout<<"\n";
-	const std::vector<int> shape = output.get_shape();
-	for (float val : shape){
-		std::cout<<val<< " ";
-	}
+//	const std::vector<int> shape = output.get_shape();
+//	for (float val : shape){
+//		std::cout<<val<< " ";
+//	}
 //	std::cout<<"\n";
 //	t_B.print();
 //	std::cout<<"\n";
