60717a93b48c39642e6b404b456deb594c2dcbdb
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-04-28 11:52:44 +0300

    BCE is ready not tested yet

--- LIB/loss.cpp
+++ LIB/loss.cpp
@@ -4,9 +4,27 @@
 #include <cmath>
 
 float Loss:BCE(Tensor outputs, Tensor labels){
+        std::vector<float> out = outputs.get_data();
+        const std::vector<int> outputs_shape = outputs.get_shape();
 
+        std::vector<float> y = labels.get_data();
+        const std::vector<int> y_shape = labels.get_shape();
 
-	return 0;
+        int batch_size = outputs_shape[0];
+
+        std::vector<float> loss_vector(batch_size);
+
+	float l = 0.0f;
+        for(int b=0; b<batch_size;++b){
+		l = -(y[b] * std::log(out[b])) + ((1-y[b]) * (std::log(1-out[b]));
+                loss_vector[b] = l;
+        }
+
+        Tensor t_loss({batch_size});
+        t_loss.from_list(loss_vector);
+
+        return t_loss;
+
 }
 
 float Loss:MSE(Tensor outputs, Tensor labels){
