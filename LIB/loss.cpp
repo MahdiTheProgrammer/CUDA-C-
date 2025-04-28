f4683f392ec2db92e94d4b6177dff325945ef02e
MahdiTheProgrammer <panahpourmohamadmahdi62@gmail.com>
2025-04-28 08:03:17 +0300

    working on MSE

--- LIB/loss.cpp
+++ LIB/loss.cpp
@@ -1,6 +1,6 @@
 // the code for the loss functions will be here
 #include "loss.h"
-
+#include <iostream>
 
 
 float Loss:BCE(Tensor outputs, Tensor labels){
@@ -10,7 +10,25 @@ float Loss:BCE(Tensor outputs, Tensor labels){
 }
 
 float Loss:MSE(Tensor outputs, Tensor labels){
+	std::vector<float> out = outputs.get_data();
+	const std::vector<int> outputs_shape = outputs.get_shape();
 
+	std::vector<float> y = labels.get_data();
+	const std::vector<int> y_shape = labels.get_shape();
+
+	int batch_size = outputs_shape[0];
+	int features = outputs_shape[1];
+
+	std::vector<float> loss_vector(batch_size);
+
+	for(int b=0; b<batch_size;++b){
+		float l = 0.0f;
+		for(int f=0; f<features; ++f){
+
+		}
+
+		loss_vector[b] = l;
+	}
 
         return 0;
 }
